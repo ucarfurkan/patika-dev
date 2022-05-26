31e22027fbdb46245dd3fa1f82204c263a945866
ucarfurkan <71367001+ucarfurkan@users.noreply.github.com>
2022-05-26 21:58:08 +0300

    Add files via upload

---
+++ ChineseZodiacCalculator/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your birth of year: ");
+        int yearOfBirth = scanner.nextInt();
+
+        int result = yearOfBirth%12;
+
+        switch (result){
+            case 0:
+                System.out.println("Monkey");
+                break;
+            case 1:
+                System.out.println("Turkey");
+                break;
+            case 2:
+                System.out.println("Dog");
+                break;
+            case 3:
+                System.out.println("Pig");
+                break;
+            case 4:
+                System.out.println("Mouse");
+                break;
+            case 5:
+                System.out.println("Ox");
+                break;
+            case 6:
+                System.out.println("Tiger");
+                break;
+            case 7:
+                System.out.println("Rabbit");
+                break;
+            case 8:
+                System.out.println("Dragon");
+                break;
+            case 9:
+                System.out.println("Snake");
+                break;
+            case 10:
+                System.out.println("Horse");
+                break;
+            case 11:
+                System.out.println("Sheep");
+                break;
+        }
+
+    }
+}
